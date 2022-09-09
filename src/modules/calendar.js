e7da368a236c82e76a3318b2a465b79fd430ffd7
sanjeev <sanjeevvsanjeev1@gmail.com>
2022-09-09 21:25:52 +0545

    update

---
+++ src/modules/calendar.js
@@ -0,0 +1,26 @@
+import {addDays, endOfWeek, isAfter, startOfWeek} from "date-fns";
+
+function takeDay(date = new Date()) {
+    let _date = date;
+    return () => {
+        _date = addDays(_date, 1);
+        return _date;
+    }
+}
+
+export function takeWeek(date = new Date()) {
+    let _date = startOfWeek(date);
+    return () => {
+        const week = [];
+        const g = takeDay(_date);
+        let day = g();
+        while (true) {
+            if (isAfter(day, endOfWeek(_date))) break;
+            week.push(day);
+            day = g();
+        }
+        _date = addDays(week.at(-1), 1);
+        return week;
+    }
+}
+
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,25 @@
+import {useState} from "react";
+import {addDays, endOfMonth, endOfWeek, format, startOfMonth, startOfWeek} from "date-fns";
+import {takeWeek} from "../modules/calendar";
+
+function buildMonth(date = new Date()) {
+    const startDate = addDays(startOfWeek(startOfMonth(date)), 1);
+    const endDate = endOfWeek(endOfMonth(date));
+    const g = takeWeek(date);
+    return [
+        g(),
+        g(),
+        g(),
+        g(),
+        g(),
+    ];
+}
+
+
 export default function HomePage() {
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const month = buildMonth(selectedDate);
 
     return (
         <div className="mt-16 container mx-auto">
@@ -8,7 +28,7 @@ export default function HomePage() {
                     className="w-full md:rounded-[20px] lg:rounded-[40px] border sm:shadow-lg bg-white text-gray-900 p-[2px] ">
                     <div
                         className="py-3 text-center bg-[#18bfe7] text-white font-black w-full  md:rounded-t-[20px] lg:rounded-t-[40px] lg:text-[40px] sm:text-[28px]">
-                        September 2022
+                        {format(selectedDate, "MMMM")} {format(selectedDate, "yyyy")}
                     </div>
                     <div className="lg:text-[24px] sm:text-[16px] my-8 text-[#444444]">
                         <div className="flex items-center w-full justify-center font-medium">
@@ -22,12 +42,21 @@ export default function HomePage() {
                             }
 
                         </div>
-                        <div className="flex items-center w-full justify-center font-light">
-                            <div
-                                className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
-                                28
-                            </div>
-                        </div>
+                        {
+                            month.map((week, index) => <div key={index}
+                                                            className="flex items-center w-full justify-center font-light">
+                                {
+                                    week.map((day, index) => <div key={index}
+                                                                  className="lg:w-[120px] lg:h-[120px] md:w-[90px] md:h-[90px] w-[60px] h-[60px] flex justify-center items-center">
+                                            {
+                                                format(new Date(), "dd")
+                                            }
+                                        </div>
+                                    )
+                                }
+                            </div>)
+                        }
+
                     </div>
                 </div>
             </div>
